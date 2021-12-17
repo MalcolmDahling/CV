e1ab714784a9fde3e21062b1a0e166a959608cc8
MalcolmDahling <mac.dahling@gmail.com>
2021-12-17 15:02:40 +0100

    Added fetch

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 document.getElementById('viewRepos').addEventListener('click', function(){
+
     document.body.insertAdjacentHTML('beforeend', `
+    <div id="backgroundOverlay2"></div>
     <div id="reposDiv">
-        <input type="button" id="reposDivClose" value="X">
+        <input id="reposDivClose" type="button" value="X">
     </div>
     `);
 
-    document.getElementById('reposDivClose').addEventListener('click', function(){
-        document.getElementById('reposDiv').remove();
-    });
+
+
+    fetch('https://api.github.com/users/MalcolmDahling/repos')
+        .then(response => response.json())
+        .then(data => fetchRepos(data));
+
+
+    function fetchRepos(data){
+        for(let i = 0; i < data.length; i++){
+
+
+
+            document.getElementById('reposDiv').insertAdjacentHTML('beforeend', `
+            <a href="` + data[i].html_url + `"`
+            + (data[i].description && data[i].description.includes('Extra övning') ? `class="extra"` : null)+`>`
+            + (data[i].description == null ? data[i].name : data[i].description)+`</a>`);
+        }
+    }
+
+    document.getElementById('reposDivClose').addEventListener('click', reposDivRemove);
+    document.getElementById('backgroundOverlay2').addEventListener('click', reposDivRemove);
 });
 
 
-document.getElementById('backgroundOverlay').addEventListener('click', function(){
+
+
+function reposDivRemove(){
     document.getElementById('reposDiv').remove();
-});
-
+    document.getElementById('backgroundOverlay2').remove();
+}
